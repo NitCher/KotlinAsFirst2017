7fe3bf3c1334bc316630714f3aa7b4a705e0abc3
Никита Черемисов <cakeDie4you@gmail.com>
2017-10-01 16:59:14 +0300

    Начал 2 урок. IfElse

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,8 +17,10 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
 
+        return (number / 1000 + (number % 1000 /100) == number % 100 /10 + number % 10 )
+}
 /**
  * Простая
  *
@@ -36,7 +38,15 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean {
+    if (x1 == x2 && y1 == y2 && r1>r2) return false
+    if (x1 == x2 && y1 == y2 && r1<r2) return true
+    if (x1 == 0.0 && x2>r1 || y1 == 0.0 && y2>r1 && r2<r1*r1 ) return false
+    if (x2 == 0.0 && x1>r2 || y2 == 0.0 && y1>r2 && r1<r2*r2) return false
+    if (x1 == x2 && y1 == y2 && r1 == r2) return true
+    if (Math.sqrt(sqr(x2 - x1) + sqr(y2 - y1)) + r1 <= r2) return true
+    else return false
+}
 
 /**
  * Средняя
